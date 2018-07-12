08d978f3fbfc197d4a3e6b6dbb4e8b562f5b3ede
RamenGuy <syssplus@hotmail.com>
2018-07-12 12:54:39 +0500

    updated

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+function Bg_Pos(){
+
+    var x = event.clientX;
+    var y = event.ClientY;
+
+    document.body.style.backgroundSize = 1300;
+    document.body.style.backgroundPositionY = 1200;
+    console.log(x + y);
+}
