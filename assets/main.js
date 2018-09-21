d2a52ee0a68bc2d7a0e442581bbf90b71994d298
DerivativeOfPiX <syssplus@hotmail.com>
2018-09-22 03:52:39 +0500

    updated for jugn0g

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,22 @@
-$(document).ready(function(){
-        $("#dynamic-img").
-        mouseover(function(){
-        	$("#dynamic-img").animate({ border-radius: '0px' }, "slow");​
-    })
-});
+$(document).ready(function() {
+
+  $("#dynamic-img").mouseover(function() {
+    	$("#dynamic-img").css("border-radius", "200px").css("transition", "1s");
+    	console.log('animated' + this.name);
+  	})
+
+  $("#dynamic-img").mouseout(function() {
+    	$("#dynamic-img").css("border-radius", "100px");
+  	})
+
+  $(".nav-item").mouseover(function(){
+		$(this).css("font-size", "30px").css("transition", "0.5s");
+		console.log('animated' + this.name);
+	})
+
+  $(".nav-item").mouseout(function(){
+		$(this).css("font-size", "20px").css("transition", "1s");
+		console.log('animated' + this.name);
+	})
+})
+
